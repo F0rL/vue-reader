3ff80624bea36ade972d11dbf3b921cb3ac0bee0
F0rl <F0rL@users.noreply.github.com>
2019-10-12 23:18:17 +0800

    init project, import icon and font

---
+++ client/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <span class="icon-close"></span>
+    <span class="icon-clock"></span>
+    <span class="text">ABCDEFG</span>
+    <span>ABCDEFG</span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Epub from "epubjs";
+global.ePub = Epub;
+
+export default {
+
+};
+</script>
+
+<style lang="scss">
+  .text {
+    font-family: 'cabin'
+  }  
+</style>
