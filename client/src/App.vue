abd4d273b011e34adb6a7169ed1b7ba0c8e278ee
F0rl <F0rL@users.noreply.github.com>
2019-10-13 02:25:19 +0800

    拆分store

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,8 +4,16 @@
   </div>
 </template>
 <script>
+import {mapGetters} from 'vuex'
 export default {
-  
+  mounted() {
+    this.$store.dispatch('setTest', 111).then(() => {
+      console.log(this.test);
+    })
+  },
+  computed: {
+    ...mapGetters(['test'])
+  }
 }
 </script>
 <style lang="scss">
