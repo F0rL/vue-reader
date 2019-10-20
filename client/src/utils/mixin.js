020327c3ced331e5fc8a0cd2ec1d346cb3e1d3ef
F0rl <F0rL@users.noreply.github.com>
2019-10-20 23:48:01 +0800

    引入组件，配置devServer，完成详情页

--- client/src/utils/mixin.js
+++ client/src/utils/mixin.js
@@ -145,7 +145,13 @@ export const storeHomeMixin = {
   methods: {
     ...mapActions(['setOffsetY', 'setHotSearchOffsetY', 'setFlapCardVisible']),
     showBookDetail(book) {
-      console.log('showbook')
+      this.$router.push({
+        path: '/store/detail',
+        query: {
+          fileName: book.fileName,
+          category: book.categoryText
+        }
+      })
     }
   }
 }
