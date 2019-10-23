e1426cefa2cd1acb8276432a7a2322274f1df1ba
F0rl <F0rL@users.noreply.github.com>
2019-10-23 22:31:30 +0800

    添加书籍进分组功能

--- client/src/utils/create-api.js
+++ client/src/utils/create-api.js
@@ -2,12 +2,12 @@ import CreateAPI from 'vue-create-api'
 import Vue from 'vue'
 import Toast from '../components/common/Toast'
 import Popup from '../components/common/Popup'
-// import GroupDialog from '../components/shelf/ShelfGroupDialog'
+import GroupDialog from '../components/shelf/ShelfGroupDialog'
 
 Vue.use(CreateAPI)
 Vue.createAPI(Toast, true)
 Vue.createAPI(Popup, true)
-// Vue.createAPI(GroupDialog, true)
+Vue.createAPI(GroupDialog, true)
 Vue.mixin({
   methods: {
     toast(settings) {
