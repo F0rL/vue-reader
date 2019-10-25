320986d3619e2a3f76d44291e3cd312460222dca
F0rl <F0rL@users.noreply.github.com>
2019-10-25 22:42:00 +0800

    初始化，完成首页接口联调

---
+++ server/const.js
@@ -0,0 +1,31 @@
+const resUrl = 'http://192.168.1.102:3000'
+
+const category = [
+  'Biomedicine',
+  'BusinessandManagement',
+  'ComputerScience',
+  'EarthSciences',
+  'Economics',
+  'Engineering',
+  'Education',
+  'Environment',
+  'Geography',
+  'History',
+  'Laws',
+  'LifeSciences',
+  'Literature',
+  'SocialSciences',
+  'MaterialsScience',
+  'Mathematics',
+  'MedicineAndPublicHealth',
+  'Philosophy',
+  'Physics',
+  'PoliticalScienceAndInternationalRelations',
+  'Psychology',
+  'Statistics'
+]
+
+module.exports = {
+  resUrl,
+  category
+}
